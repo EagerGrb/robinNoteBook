5efc006252688e8471d678648f60c880058449d1
EagerGrb <282113895@qq.com>
2023-01-19 14:44:36 +0800

    build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,15 +4,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'robinNoteBook',
+  tagline: '快乐mil mil 屋',
+  url: 'https://eagergrb.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Eager', // Usually your GitHub org/user name.
+  projectName: 'robinNoteBook', // Usually your repo name.
 
   presets: [
     [
